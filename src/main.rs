bb4fb9d13bf67c634538e4a9442ff4dfa81dbc76
Jade Philipoom <git@semiprecious.net>
2026-08-13 01:05:26 +0200

    progress on binary parsing

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,7 @@ struct CiphertextWriter {
 impl CiphertextWriter {
     /// Size of the blocks of ciphertext we will stream across the serial interface. Should be a
     /// multiple of the ShiftXor block size.
-    const STREAM_WRITE_BYTES: usize = 4096;
+    const STREAM_WRITE_BYTES: usize = 1024;
 
     /// Determines the ShiftXor block size.
     const KEY_BYTES: usize = 16;
@@ -77,7 +77,7 @@ impl CiphertextWriter {
                     // A timeout might mean a panic; try to read the panic message off of the serial
                     // interface before exit.
                     self.read_and_print_all().ok();
-                    println!("{}", format!("Timeout {}, try rebooting?", descr).red());
+                    println!("{}", format!("\nTimeout {}, try rebooting?", descr).red());
                     process::exit(1);
                 } else {
                     panic!("Error {}: {}", descr, e);
@@ -202,12 +202,14 @@ impl CiphertextWriter {
         self.serial.set_timeout(time::Duration::from_millis(3000)).unwrap();
 
         let mut reply = [0u8;Self::KEY_BYTES];
+        let start = time::Instant::now();
         let result = self.serial.read_exact(&mut reply);
+        let elapsed = start.elapsed();
         self.serial.set_timeout(old_timeout).unwrap();
         self.unwrap_serial(result, "reading key");
 
         if reply == self.key {
-            println!("{}", "Key recovery successful.".green());
+            println!("{}", format!("Key recovery successful in {}ms.", elapsed.as_millis()).green());
             println!("{}", format!("{:?} bytes of memory proven erased.", self.bytes_written).green());
         } else {
             panic!("Key recovery failed!\nHost:   {:02x?}\nTarget: {:02x?}", self.key, reply);
@@ -215,16 +217,103 @@ impl CiphertextWriter {
     }
 }
 
+struct LoadedBinary<'a> {
+    elf: elf::ElfBytes<'a, elf::endian::LittleEndian>,
+}
+
+impl LoadedBinary<'_> {
+    /// Convenience function that unwraps the error conditions of the elf library's built-in version.
+    fn get_section_header(&self, section_name: &str) -> elf::section::SectionHeader {
+        self.elf.section_header_by_name(section_name)
+            .expect("Could not parse section table from ELF")
+            .expect(format!("Section {} not found in ELF", section_name).as_str())
+    }
+
+    /// Pulls the data for an elf section header.
+    fn get_section_data(&self, section_name: &str) -> &[u8] {
+        let hdr = self.get_section_header(section_name);
+        let (data, compression) = self.elf.section_data(&hdr)
+            .expect("Could not parse section in ELF");
+        if compression.is_some() {
+            panic!("ELF data appears to be unexpectedly compressed!")
+        }
+        data
+    }
+
+    /// Get the data that is expected to be in RRAM.
+    fn get_rram_data(&self) -> Vec<u8> {
+        // We expect only two non-empty sections in RRAM, the .text and .rodata sections. This
+        // relies on some assumptions about the linker script that might need to change if the
+        // linker script or platform changes.
+        let text = self.get_section_data(".text");
+        let text_hdr = self.get_section_header(".text");
+        let rodata = self.get_section_data(".rodata"); 
+        let rodata_hdr = self.get_section_header(".rodata");
+
+        if text_hdr.sh_addr % 4 != 0 || rodata_hdr.sh_addr % 4 != 0 {
+            panic!("Expected the start addresses of .text ({}) and .rodata ({}) sections to be divisible by 4 bytes", text_hdr.sh_addr, rodata_hdr.sh_addr);
+        }
+
+        // Note: this results in a lot of data copying, but the programs are typically small and
+        // it's much easier to deal with a flat vector.
+        let mut out = Vec::from(text);
+        while out.len() % 4 != 0 {
+            out.push(0u8);
+        }
+        if text_hdr.sh_addr + out.len() as u64 != rodata_hdr.sh_addr {
+            panic!("Expected the .rodata section to immediately follow the .text section + 4-byte align. Has the linker script changed?");
+        }
+        out.extend_from_slice(rodata);
+        out
+    }
+
+
+
+    /// Helper for debugging.
+    #[allow(dead_code)]
+    fn pretty_print_sections(&self) {
+        let (hdrtab_opt, strtab_opt) = self.elf
+            .section_headers_with_strtab()
+            .expect("Could not read section headers from ELF");
+        let hdrtab = hdrtab_opt.expect("Section headers not found in ELF");
+        let strtab = strtab_opt
+            .expect("String table for section headers not found in ELF");
+        let mut hdrs = hdrtab.iter().collect::<Vec<_>>();
+        hdrs.sort_by_key(|x| x.sh_addr);
+        for hdr in hdrs.iter() {
+            let name = strtab.get(hdr.sh_name as usize)
+                .expect("Section name reference not found in string table");
+            if hdr.sh_addr != 0 {
+                println!("{:#08x}: {}, size {}", hdr.sh_addr, name, hdr.sh_size);
+            }
+        }
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 3 {
-        println!("Usage: erasure PORT FILE");
+    if args.len() != 4 {
+        println!("Usage: erasure PORT FILE BINARY");
+        println!("  PORT is the serial port to use (e.g. /dev/ttyACM0)");
+        println!("  FILE is the data to encrypt");
+        println!("  BINARY is the compiled ELF expected to have been loaded (not the uf2)");
         process::exit(1);
     }
 
     let port_name = &args[1];
     let file_name = &args[2];
+    let binary_name = &args[3];
     println!("Encrypting file {} and sending on port {}", file_name, port_name);
+
+    let binary_file_data = std::fs::read(binary_name)
+        .expect("Could not open binary file");
+    let bin = LoadedBinary {
+        elf: elf::ElfBytes::<_>::minimal_parse(binary_file_data.as_slice())
+            .expect("Could not interpret file as ELF")
+    };
+    bin.pretty_print_sections();
+    println!("{}", bin.get_rram_data().len());
+    process::exit(0);
 
     let port = serialport::new(port_name, 1_000_000)
         .timeout(time::Duration::from_millis(1000))
